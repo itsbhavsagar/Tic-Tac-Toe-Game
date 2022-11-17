63d1debed0eed2c7558c5f17fe5306bf672d3e7b
Bhavsagar <bhavsagar@protonmail.com>
2022-11-17 21:07:11 +0530

    removed "error message when closed and open again"

--- js/config.js
+++ js/config.js
@@ -1,22 +1,24 @@
 function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
 }
 
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
-function savePlayerConfig (event) {
-event.preventDefault();
-const formData = new FormData(event.target);
-const enteredPlayername = formData.get('playername').trim();    
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get('playername').trim();
 
-if (!enteredPlayername) { // enteredPlayername === ''
+  if (!enteredPlayername) {
+    // enteredPlayername === ''
     event.target.firstElementChild.classList.add('error');
-errorsOutputElement.textContent = 'Enter a valid name!';
-return;
-}
-
+    errorsOutputElement.textContent = 'Enter a valid name!';
+    return;
+  }
 }
