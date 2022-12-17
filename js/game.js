c3236daa2063dbef83e6763b66d12cf239cb9775
Bhavsagar <bhavsagar@protonmail.com>
2022-12-17 09:55:01 +0530

    //

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+'use Strict';
+
 function startNewGame() {
   if (players[0].name === '' || players[1] === '') {
     alert('Please set custom player names for both players!');
@@ -17,15 +19,23 @@ function switchPlayer() {
   activePlayerNameElement.textContent = players[activePlayer].name;
 }
 
-
 function selectGameField(event) {
-  if (event.target.tagName !== 'LI'){
+  if (event.target.tagName !== 'LI') {
     return;
   }
-}
 
-function selectGameField(event) {
+
+const selectedField = event.target;
+
+
   event.target.textContent = players[activePlayer].symbol; // players [0]
   event.target.classList.add('disabled');
+
+  const selectedColumn = selectedField.dataset.col -1;
+  const selectedRow = selectedField.dataset.row -1;
+
+  gameData[selectedRow],[selectedColumn];
+
   switchPlayer();
+
 }
